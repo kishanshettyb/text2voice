2feba7162234e14cdd3a2eb8bcde28192314f7e8
kishanshettyb <kishanqr@gmail.com>
2025-02-24 14:41:20 +0530

    feat: 🎸 add app auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
+import { LoginForm } from '@/components/login'
 
 function Page() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="p-4 w-[400px] h-[200px] border rounded-2xl shadow-2xl shadow-green-100">
-        <Link href="/studio">
-          <Button>Login</Button>
-        </Link>
-      </div>
+      <LoginForm />
     </div>
   )
 }
