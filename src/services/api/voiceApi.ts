2bcbe0d041315cc73aadb6221bf533e200cbd771
kishanshettyb <kishanqr@gmail.com>
2025-03-06 20:23:20 +0530

    fix: 🐛 fix code

--- src/services/api/voiceApi.ts
+++ src/services/api/voiceApi.ts
@@ -13,24 +13,3 @@ export const getAllLanguages = async () => {
   }
   return response.json()
 }
-
-// interface VoiceProps {
-//   languge: string;
-//   text: string;
-//   voice: string;
-// }
-
-// export const createVoicePaly = async (data: VoiceProps) => {
-//   const response = await fetch("/api/new", {
-//     method: "POST",
-//     body: JSON.stringify(data), // Use the provided data directly
-//     headers: { "Content-Type": "application/json" },
-//   });
-
-//   if (!response.ok) {
-//     // Handle errors appropriately
-//     throw new Error(`HTTP error! status: ${response.status}`);
-//   }
-
-//   return response.json();
-// };
