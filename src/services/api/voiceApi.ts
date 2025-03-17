080a428bf0bcbef7a9feb69a90659552d2e8f361
kishanshettyb <kishanqr@gmail.com>
2025-03-17 11:32:18 +0530

    feat: 🎸 add dynamic files fetch for myfiles data table

--- src/services/api/voiceApi.ts
+++ src/services/api/voiceApi.ts
@@ -1,3 +1,7 @@
+import axios from 'axios'
+
+// import Cookies from "js-cookie";
+
 export const getAllVoices = async () => {
   const response = await fetch('../api/voices')
   if (!response.ok) {
@@ -12,4 +16,19 @@ export const getAllLanguages = async () => {
     throw new Error('Failed to fetch voices')
   }
   return response.json()
+}
+
+// const token = Cookies.get('token');
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_ADMIN_TOKEN}`
+  }
+})
+
+export const getAllUserTextToVoiceData = async (userId: string) => {
+  const response = await axiosInstance.get(
+    `text-to-voice-generations?filters[user][documentId][$eq]=${userId}&populate=user&pagination[pageSize]=10`
+  )
+  return response // ✅ Extract and return only the data
 }
