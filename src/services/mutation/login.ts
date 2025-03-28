ba61680e2cbcf6d8b4adfe46c89b5a680f6d518a
kishanshettyb <kishanqr@gmail.com>
2025-03-28 20:06:35 +0530

    fix: 🐛 fix login component

--- src/services/mutation/login.ts
+++ src/services/mutation/login.ts
@@ -1,24 +1,38 @@
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import Cookies from 'js-cookie'
 
 export const useLoginMutation = () => {
   const router = useRouter()
+  const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL
+  if (!LOGIN_URL) {
+    throw new Error('LOGIN_URL is not defined in the environment variables.')
+  }
 
   return useMutation({
     mutationFn: async (loginData) => {
-      await axios.post('../../api/login', loginData, {
-        withCredentials: true // Ensures the cookie is sent
+      const response = await axios.post(LOGIN_URL, loginData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
       })
+      return response
     },
-    onSuccess: () => {
+    onSuccess: async (data) => {
       router.push('/studio')
+      Cookies.set('token', data?.data.jwt, { expires: 1, path: '/' })
+      Cookies.set('userId', data?.data.user.documentId, {
+        expires: 1,
+        path: '/'
+      })
     },
     onError: (error) => {
       console.error(error.response?.data?.error || error.message)
     }
   })
 }
+
 export const useLogoutMutation = () => {
   const router = useRouter()
 
