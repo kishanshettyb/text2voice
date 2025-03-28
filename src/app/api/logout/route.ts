ad12bf60d6512d8fc0c89e18af9f7277e6c67a63
kishanshettyb <kishanqr@gmail.com>
2025-03-28 19:55:42 +0530

    fix: 🐛 fix logout functionality

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,5 +3,6 @@ import { NextResponse } from 'next/server'
 export async function POST() {
   const res = NextResponse.json({ message: 'Logged out' })
   res.cookies.set('token', '', { maxAge: 0 }) // Clear cookie
+  res.cookies.set('userId', '', { maxAge: 0 }) // Clear cookie
   return res
 }
