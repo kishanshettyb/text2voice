ecac93750c67db4749c72b684989d518a1773ae3
kishanshettyb <kishanqr@gmail.com>
2025-03-29 08:02:59 +0530

    refactor: 💡 refactor voice generator code

--- src/components/voiceGenerator.tsx
+++ src/components/voiceGenerator.tsx
@@ -19,78 +19,78 @@ import { useParams } from 'next/navigation'
 import { TitleBar } from '@/components/titleBar'
 import useTitleStore from '@/store/title'
 import Cookies from 'js-cookie'
+import { generateSpeech, uploadTextToSpeech } from '@/services/api/generateSpeech'
 
 // Form schema using Zod for validation
 const formSchema = speechSchema
-interface RequestData {
-  text: string
-  speed: string
-  userId: string | undefined
-}
-interface SendData {
-  uid: string
-  voices: string
-  token: string
-  title: string
-  userId: string
-}
+// interface RequestData {
+//   text: string
+//   speed: string
+//   userId: string | undefined
+// }
+// interface SendData {
+//   uid: string
+//   voices: string
+//   token: string
+//   title: string
+//   userId: string
+// }
 
 // Function to call the API and generate the speech
-const generateSpeech = async (requestData: RequestData) => {
-  const response = await fetch('../../api/tts', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(requestData)
-  })
+// const generateSpeech = async (requestData: RequestData) => {
+//   const response = await fetch('../../api/tts', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(requestData)
+//   })
 
-  const data = await response.json()
-  console.log(data)
-  if (!response.ok || !data) {
-    throw new Error('Failed to generate speech')
-  }
-  return data
-}
+//   const data = await response.json()
+//   console.log(data)
+//   if (!response.ok || !data) {
+//     throw new Error('Failed to generate speech')
+//   }
+//   return data
+// }
 
-const uploadTextToSpeech = async (sendData: SendData) => {
-  const userId = Cookies.get('userId')
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/text-to-voice-generations`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${sendData.token}`
-    },
-    body: JSON.stringify({
-      data: {
-        uid: sendData.uid,
-        voices: sendData.voices,
-        title: sendData.title,
-        users_permissions_user: userId
-      }
-    })
-  })
+// const uploadTextToSpeech = async (sendData: SendData) => {
+//   const userId = Cookies.get('userId')
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/text-to-voice-generations`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${sendData.token}`
+//     },
+//     body: JSON.stringify({
+//       data: {
+//         uid: sendData.uid,
+//         voices: sendData.voices,
+//         title: sendData.title,
+//         users_permissions_user: userId
+//       }
+//     })
+//   })
 
-  const data = await response.json()
-  if (!response.ok || !data) {
-    throw new Error('Failed to generate speech')
-  }
-  return data
-}
+//   const data = await response.json()
+//   if (!response.ok || !data) {
+//     throw new Error('Failed to generate speech')
+//   }
+//   return data
+// }
 
 function VoiceGenerator() {
   const fileTitle = useTitleStore((state) => state.title) || 'United'
   const params = useParams()
   const [pageUid, setPageUid] = useState('')
-  const uid = params?.uid // Extract 'uid' from params
+  const uid = params?.uid
   const [textCount, setTextCount] = useState(0)
-  useEffect(() => {
-    console.log('Extracted UID:', uid)
-    setPageUid(uid)
-  }, [uid])
-
   const [isOpen, setIsOpen] = useState(false)
   const voiceSpeed = useVoiceStore((state) => state.voiceSpeed) || '1.0x'
   const [audioUrl, setAudioUrl] = useState<string>('')
   const [audioText, setAudioText] = useState<string>('')
+
+  useEffect(() => {
+    setPageUid(uid)
+  }, [uid])
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -100,24 +100,17 @@ function VoiceGenerator() {
     mode: 'onChange'
   })
 
-  // Use TanStack Query's useMutation for generating speech
   const { mutateAsync, isPending, isError, error } = useMutation({
     mutationFn: generateSpeech,
     onSuccess: (data) => {
       setAudioUrl(data.documentId)
       setAudioText(data.text)
-
-      const uid = pageUid
-      const token = data.token
-      const title = fileTitle
-      const voices = data.documentId
-      const userId = data.userId
       const sendData = {
-        uid,
-        token,
-        voices,
-        title,
-        userId
+        uid: pageUid,
+        token: data.token,
+        voices: data.documentId,
+        title: fileTitle,
+        userId: data.userId
       }
       uploadTextToSpeech(sendData)
     }
@@ -134,21 +127,16 @@ function VoiceGenerator() {
     mutateAsync(requestData)
   }
 
-  // Update title in Zustand store based on the first 50 words in the textarea
   const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const text = event.target.value
-    const title = text.slice(0, 30) // Take only the first 10 characters
+    const title = text.slice(0, 30) // Limit to 30 chars for title
     useTitleStore.getState().settitle(title)
-    console.log(text.length)
-    let textLength = text.length
-    if (textLength > 5000) {
-      textLength = 5000
-    }
-    setTextCount(textLength)
+    setTextCount(text.length > 5000 ? 5000 : text.length)
   }
+
   return (
     <div className="flex flex-col md:flex-row gap-x-6 gap-y-6 lg:gap-y-0 pt-3">
-      <div className={`w-full ${audioUrl ? `md:w-3/4>` : ``}`}>
+      <div className={`w-full ${audioUrl ? 'md:w-3/4>' : ''}`}>
         <TitleBar />
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -205,7 +193,7 @@ function VoiceGenerator() {
                     <p className="text-xs opacity-50">3900 credits remaining</p>
                   </div>
                   <div
-                    className={`flex justify-end gap-x-5 ${isError ? `mt-0` : `mt-5`} items-center flex-row`}
+                    className={`flex justify-end gap-x-5 ${isError ? 'mt-0' : 'mt-5'} items-center flex-row`}
                   >
                     <div className="w-full">
                       <p className="text-xs opacity-50">{textCount} / 5,000 characters</p>
@@ -219,12 +207,12 @@ function VoiceGenerator() {
                         {isPending ? (
                           <>
                             <Loader2 className="animate-spin" size={30} />
-                            {audioUrl ? `Regenerating...` : `Generating...`}
+                            {audioUrl ? 'Regenerating...' : 'Generating...'}
                           </>
                         ) : (
                           <>
                             <Zap size={24} />
-                            {audioUrl ? `Regenerate Speech` : `Generate Speech`}
+                            {audioUrl ? 'Regenerate Speech' : 'Generate Speech'}
                           </>
                         )}
                       </Button>
